c4b4ae84379b51677c38e5bd321f2c7bbafd8f85
anaestevam <anaestevam9@gmail.com>
2023-06-28 20:27:53 -0300

    falta ajeitar

---
+++ rt3/build/CMakeFiles/basic_rt3.dir/src/core/lights/light.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/basic_rt3.dir/src/core/lights/light.cpp.o: \
+ /home/ana/RayTracer/rt3/src/core/lights/light.cpp \
+ /usr/include/stdc-predef.h
---
+++ rt3/build/CMakeFiles/basic_rt3.dir/src/core/surfaceinteraction.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/basic_rt3.dir/src/core/surfaceinteraction.cpp.o: \
+ /home/ana/RayTracer/rt3/src/core/surfaceinteraction.cpp \
+ /usr/include/stdc-predef.h
